18ca8930c1d426a09cb94295518ec25bfadc4fc6
vcAndrew <a_vu16@u.pacific.edu>
2022-03-28 07:24:36 -0700

    Added sample quest text
    Currently uses PAUSE button

--- Assets/Scripts/ScreenTransition.cs
+++ Assets/Scripts/ScreenTransition.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;	//need for text
 using UnityEngine.SceneManagement;
 
 public class ScreenTransition : MonoBehaviour
 {
+	public Text quest_text;
+
 	public void Switch_to_Game()
 	{
 		SceneManager.LoadScene("GameScene");
@@ -18,5 +21,17 @@ public class ScreenTransition : MonoBehaviour
 	public void Switch_to_Main_Menu()
 	{
 		SceneManager.LoadScene("MainMenu");
+	}
+
+	//testing text change
+	public void Update_Text()
+	{
+		quest_text.text = "NEW QUEST";
+		/*
+		if (Input.GetKey(KeyCode.Q))
+       		{
+			quest_text.text = "NEW QUEST";
+        	}
+		*/
 	}
 }
